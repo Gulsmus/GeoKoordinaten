852d590e39bc11837cd1162d0b7b039b46bc298e
Fabian R <fabianr@deb04.quadress.de>
2024-12-18 17:03:52 +0100

    Finale logische Anpassungen

--- venv/Gpx Viewer.py
+++ venv/Gpx Viewer.py
@@ -1,52 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 import os
 import gpxpy
 import gpxpy.gpx
 import math
 from datetime import datetime, date
+from models import db_connect, create_table, Track, Coordinate, Driver, Vehicle
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///gpx_data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/gpx_data.db'
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-# Database models
-class Track(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(150), nullable=True)
-    file_path = db.Column(db.String(200), nullable=False)
-    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'), nullable=False)
-    driver_id = db.Column(db.Integer, db.ForeignKey('driver.id'), nullable=False)
-    date = db.Column(db.Date, nullable=True)
-    start_time = db.Column(db.DateTime, nullable=True)
-    end_time = db.Column(db.DateTime, nullable=True)
-
-    coordinates = db.relationship('Coordinate', backref='track', lazy=True)
-    driver = db.relationship('Driver', back_populates='track')
-    vehicle = db.relationship('Vehicle', back_populates='track')
-
-class Coordinate(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    lat = db.Column(db.Float, nullable=False)
-    lon = db.Column(db.Float, nullable=False)
-    speed = db.Column(db.Float, nullable=True)
-    time = db.Column(db.DateTime, nullable=True)
-    track_id = db.Column(db.Integer, db.ForeignKey('track.id'), nullable=False)
-
-class Driver(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False)
-
-    track = db.relationship('Track', back_populates='driver')
-class Vehicle(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=False)
-
-    track = db.relationship('Track', back_populates='vehicle')
-
+session = sessionmaker(bind=db_connect())()
+create_table(db_connect())
 # Ensure the upload folder exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
@@ -58,22 +26,38 @@ def parse_gpx(file_path):
         coordinates = []
         start_time = None
         end_time = None
-        trackname = 'Unbekannt'
-
+        
         print(gpx)
+        tracks = []
         for waypoint in gpx.waypoints:
-            coordinates.append((waypoint.latitude,waypoint.longitude,0.0,waypoint.time))
+            coordinates.append((waypoint.latitude,waypoint.longitude,waypoint.elevation,0.0,waypoint.time))
+            trackname = waypoint.name if not waypoint.name is None else 'Wegpunkte'
+            if start_time is None or waypoint.time < start_time:
+               start_time = waypoint.time
+            if end_time is None or waypoint.time > end_time:
+               end_time = waypoint.time
+        if not coordinates == []:
+          if trackname != 'Wegpunkte' and trackname[0].isdigit() and trackname[-1].isdigit():
+            trackname = 'Wegpunkte'
+          tracks.append((coordinates,start_time,end_time,trackname,'Wegpunkt')) 
+
         for track in gpx.tracks:
-            trackname = track.name if not None else trackname
+            coordinates = []
+            start_time = None
+            end_time = None
+            trackname = track.name if not track.name is None else 'Strecke'
             for segment in track.segments:
                 for point in segment.points:
-                    coordinates.append((point.latitude, point.longitude, point.speed, point.time))
+                    coordinates.append((point.latitude, point.longitude,point.elevation, point.speed, point.time))
                     if start_time is None or point.time < start_time:
                         start_time = point.time
                     if end_time is None or point.time > end_time:
                         end_time = point.time
+            if trackname != 'Strecke' and trackname[0].isdigit() and trackname[-1].isdigit():
+              trackname = 'Strecke'
+            tracks.append((coordinates,start_time,end_time,trackname,'Strecke'))
 
-        return coordinates, start_time, end_time, trackname
+        return tracks#coordinates, start_time, end_time, trackname
 
 def haversine(lat1, lon1, lat2, lon2):
     """Calculate the great-circle distance between two points on the Earth."""
@@ -87,11 +71,32 @@ def haversine(lat1, lon1, lat2, lon2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
     return R * c
+    
+def calculate_total_distance(coordinates):
+  total_distance = 0
+  if len(coordinates) > 1:
+    distances = []
+    for i in range(1, len(coordinates)):
+       lat1, lon1 = coordinates[i-1][0], coordinates[i-1][1]
+       lat2, lon2 = coordinates[i][0], coordinates[i][1]
+       distance = haversine(lat1, lon1, lat2, lon2)
+       distances.append(distance)
+    total_distance = round(sum(distances),2)
+    
+  return total_distance
+  
+def calculate_avg_speed(total_distance, start_time, end_time):
+  avg_speed = 0
+  if total_distance != 0 and start_time is not None and end_time is not None:
+    track_time = (end_time - start_time).total_seconds()/3600 #Umwandlung von Sekunden in Stunden
+    print(track_time)
+    avg_speed = round(total_distance/track_time,2)
+  return avg_speed
 
 @app.route('/')
 def index():
-    vehicles = db.session.query(Vehicle.name).distinct().all()
-    drivers = db.session.query(Driver.name).distinct().all()
+    vehicles = session.query(Vehicle.name).distinct().all()
+    drivers = session.query(Driver.name).distinct().all()
     return render_template('index.html', vehicles=vehicles, drivers=drivers)
 
 @app.route('/upload', methods=['POST'])
@@ -105,39 +110,47 @@ def upload_file():
 
     if file:
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file_check = db.session.query(Track).filter(Track.file_path == file_path).first()
+        file_check = session.query(Track).filter(Track.file_path == file_path).first()
         if file_check:
           return redirect(url_for('index'))
         else:
           file.save(file_path)
 
           # Parse GPX and save data to database
-          coordinates, start_time, end_time, track_name = parse_gpx(file_path)
-          splitted_filename = file.filename.split('_')
-          driver_name = splitted_filename[0]#request.form.get('vehicle')
-          vehicle_name = splitted_filename[1]#request.form.get('driver')
+          for coordinates,start_time,end_time,track_name,type in parse_gpx(file_path):
+#          coordinates, start_time, end_time, track_name = parse_gpx(file_path)
+            splitted_filename = file.filename.split('_')
+            driver_name = splitted_filename[0]#request.form.get('vehicle')
+            vehicle_name = splitted_filename[1]#request.form.get('driver')
 
-          vehicle = db.session.query(Vehicle).filter(Vehicle.name == vehicle_name).first()
-          driver = db.session.query(Driver).filter(Driver.name == driver_name).first()
-          if not driver:
-            driver = Driver(name=driver_name)
-            db.session.add(driver)
-            db.session.flush()
-          if not vehicle:
-            vehicle = Vehicle(name=vehicle_name)
-            db.session.add(vehicle)
-            db.session.flush()
-          #date = request.form.get('date')
-          track_date = date(start_time.year,start_time.month,start_time.day) if not start_time is None else None
+            vehicle = session.query(Vehicle).filter(Vehicle.name == vehicle_name).first()
+            driver = session.query(Driver).filter(Driver.name == driver_name).first()
+            if not driver:
+              driver = Driver(name=driver_name)
+              session.add(driver)
+              session.flush()
+            if not vehicle:
+              vehicle = Vehicle(name=vehicle_name)
+              session.add(vehicle)
+              session.flush()
+            #date = request.form.get('date')
+            track_date = date(start_time.year,start_time.month,start_time.day) if not start_time is None else None
+            
+            total_distance =0
+            avg_speed=0
+            if len(coordinates) > 1:
+              total_distance = calculate_total_distance(coordinates)
+              if type == 'Strecke':
+                avg_speed = calculate_avg_speed(total_distance, start_time, end_time)
 
-          track = Track(name=track_name, file_path=file_path, vehicle_id=vehicle.id, driver_id=driver.id, date=track_date, start_time=start_time, end_time=end_time)
-          db.session.add(track)
-          db.session.commit()
+            track = Track(name=track_name, file_path=file_path, vehicle_id=vehicle.id, driver_id=driver.id, date=track_date,total_distance= total_distance,avg_speed=avg_speed, start_time=start_time, end_time=end_time)
+            session.add(track)
+            session.commit()
 
-          for lat, lon, speed, time in coordinates:
-              coord = Coordinate(lat=lat, lon=lon, speed=speed, time=time, track_id=track.id)
-              db.session.add(coord)
-          db.session.commit()
+            for lat, lon,ele, speed, time in coordinates:
+                coord = Coordinate(lat=lat, lon=lon, ele=ele,speed=speed, time=time, track_id=track.id)
+                session.add(coord)
+            session.commit()
 
           return redirect(url_for('index'))
 
@@ -148,7 +161,7 @@ def filter_tracks():
     date_from = request.form.get('date_from')
     date_to = request.form.get('date_to')
 
-    query = Track.query
+    query = session.query(Track)
     if vehicle_name:
         query = query.join(Vehicle).filter(Vehicle.name==vehicle_name)
     if driver_name:
@@ -163,10 +176,10 @@ def filter_tracks():
 
 @app.route('/track/<int:track_id>')
 def view_track(track_id):
-    track = Track.query.get_or_404(track_id)
+    track = session.query(Track).filter(Track.id == track_id).first()
     coordinates = [{'lat': coord.lat, 'lon': coord.lon, 'speed': coord.speed, 'time': coord.time} for coord in track.coordinates]
-    total_distance = 0
-    avg_speed = 0
+    total_distance = track.total_distance
+    avg_speed = track.avg_speed
 
     if len(coordinates) > 1:
         distances = []
@@ -182,10 +195,10 @@ def view_track(track_id):
             avg_speed = sum(valid_speeds) / len(valid_speeds)
             avg_speed = round(avg_speed,2)
 
-    return render_template('view_track.html', track=track, coordinates=coordinates, total_distance=total_distance, avg_speed=avg_speed)
+    return render_template('view_track.html', track=track, driver= track.driver, vehicle=track.vehicle,coordinates=coordinates, total_distance=total_distance, avg_speed=avg_speed)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-    app.run(debug=True)
+    #with app.app_context():
+        #db.drop_all()
+        #db.create_all()
+    app.run(host='0.0.0.0',debug=True,port=5000)
